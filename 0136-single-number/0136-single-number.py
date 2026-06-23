38cf7a628e4ef3f515a7a54e361bf087f760d2bc
Rahul-Sarang <ars.rahul.san@gmail.com>
2026-06-23 14:43:56 +0530

    Time: 3 ms (61.27%), Space: 21.2 MB (29.28%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,6 @@
-from functools import reduce
-from operator import xor
-
-  
 class Solution:
-    def singleNumber(self, nums):
-        return reduce(xor, nums)
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
