cf45d3424ae569afc01fd21a4d1a34ed7a1b1bf1
Rahul-Sarang <ars.rahul.san@gmail.com>
2026-06-24 10:40:52 +0530

    Time: 0 ms (100%), Space: 19.8 MB (53.5%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -10,5 +10,7 @@ class Solution:
                 right -= 1
             else:
                 nums[left], nums[right] = nums[right], nums[left]
+                left+=1
+                right-=1
 
         return nums
