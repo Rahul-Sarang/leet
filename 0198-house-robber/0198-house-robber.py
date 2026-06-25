06fd73b15af549dfe47a87f1b9b9fb0be78e252e
Rahul-Sarang <ars.rahul.san@gmail.com>
2026-06-25 09:55:06 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,13 @@
 class Solution(object):
     def rob(self, nums):
         dp = {}
-
         def helper(i):
             if i >= len(nums):
                 return 0
-
             if i in dp:
                 return dp[i]
-
             robCurrent = nums[i] + helper(i + 2)
             skipCurrent = helper(i + 1)
-
             dp[i] = max(robCurrent, skipCurrent)
             return dp[i]
-
         return helper(0)
